1f3e16697cc8e088a6746b8e53c7ad18d9b8e492
Richard Weiss <richardweiss@richardweiss.org>
2016-10-08 17:37:06 +1100

    all data sources

--- outbrain/data_sources.py
+++ outbrain/data_sources.py
@@ -1,8 +1,27 @@
 import luigi
-from plumbum import local
 import luigi.file
+import luigi.s3
+from plumbum import local
 import outbrain.config
 import os
+
+class FetchS3Data(luigi.Task):
+    def requires(self):
+        yield FetchS3ZipFile(name='clicks_test.csv.zip')
+        yield FetchS3ZipFile(name='clicks_train.csv.zip')
+        yield FetchS3ZipFile(name='documents_categories.csv.zip')
+        yield FetchS3ZipFile(name='documents_entities.csv.zip')
+        yield FetchS3ZipFile(name='documents_meta.csv.zip')
+        yield FetchS3ZipFile(name='documents_topics.csv.zip')
+        yield FetchS3ZipFile(name='events.csv.zip')
+        yield FetchS3ZipFile(name='page_views.csv.zip')
+        yield FetchS3ZipFile(name='page_views_sample.csv.zip')
+        yield FetchS3ZipFile(name='promoted_content.csv.zip')
+        yield FetchS3ZipFile(name='sample_submission.csv.zip')
+
+    def run(self):
+        pass
+
 
 class FetchS3ZipFile(luigi.Task):
     file_name = luigi.Parameter()
@@ -15,7 +34,9 @@ class FetchS3ZipFile(luigi.Task):
 
     def run(self):
         client = luigi.s3.S3Client()
-        path = self.output().name
+        path = self.output().path
         out = client.get('s3://riri-machine-learning/{}'.format(self.file_name), path)
-        with local.cwd(config.working_dir):
+        with local.cwd(outbrain.config.working_dir):
             local['unzip'](self.file_name)
+
+
